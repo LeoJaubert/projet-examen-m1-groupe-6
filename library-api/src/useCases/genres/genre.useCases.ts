a1cd595b837d7137b03cde2196ebf20aba5b4f7a
pierre-antoine.gilbert@student.junia.com <pierre-antoine.gilbert@student.junia.com>
2023-11-07 09:22:39 +0100

    ESLint fix.

--- library-api/src/useCases/genres/genre.useCases.ts
+++ library-api/src/useCases/genres/genre.useCases.ts
@@ -3,11 +3,10 @@ import { GenreRepository } from 'library-api/src/repositories';
 import { GenreModelOutput } from 'library-api/src/useCases/genres/genre.useCases.type';
 
 @Injectable()
-export class GenreUseCases 
-{
-    constructor(private readonly genreRepository: GenreRepository) {}
+export class GenreUseCases {
+  constructor(private readonly genreRepository: GenreRepository) {}
 
-    public async getAllPlain(): Promise<GenreModelOutput[]> {
-        return this.genreRepository.getAllPlain();
-      }
+  public async getAllPlain(): Promise<GenreModelOutput[]> {
+    return this.genreRepository.getAllPlain();
+  }
 }
