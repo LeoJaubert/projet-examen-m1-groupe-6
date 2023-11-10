cb7adc35680936ee132ad79eb394b36692b20180
pierre-antoine.gilbert@student.junia.com <pierre-antoine.gilbert@student.junia.com>
2023-11-10 17:51:12 +0100

    Page des utilisateurs.

--- library-site/src/app/users/[id]/page.tsx
+++ library-site/src/app/users/[id]/page.tsx
@@ -1,17 +1,26 @@
 'use client';
 
 import { useParams } from 'next/navigation';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useUsersProviders } from '@/hooks';
 
 const UserDetailsPage: FC = () => {
   const { id } = useParams();
 
+  const { useListUsers } = useUsersProviders();
+  const { users, load } = useListUsers();
+  useEffect(() => load, [load]);
+
+  const user = users.find((elem) => elem.id === id);
+
   return (
-    <>
-      User details &apos;
-      {id}
-      &apos; not implemented
-    </>
+    <main>
+      <p className="text-lg font-semibold">
+        {`${user?.firstname} ${user?.lastname}`}
+      </p>
+
+      {user?.books.map((book) => <p>{`${book}`}</p>)}
+    </main>
   );
 };
 
