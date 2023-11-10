8ea12370308a57a9feb72fdd6c7ac41d487776ed
pierre-antoine.gilbert@student.junia.com <pierre-antoine.gilbert@student.junia.com>
2023-11-10 17:42:45 +0100

    Page de détail des livres.

--- library-site/src/app/books/[id]/page.tsx
+++ library-site/src/app/books/[id]/page.tsx
@@ -2,23 +2,30 @@
 
 import { useParams } from 'next/navigation';
 import { FC, useEffect } from 'react';
-import { useBooksProviders } from '@/hooks';
+import { useBooksProviders, useUsersProviders } from '@/hooks';
 
 const BooksDetailsPage: FC = () => {
   const { id } = useParams();
 
   const { useListBooks } = useBooksProviders();
   const { books, loadBooks } = useListBooks();
-
   useEffect(() => loadBooks, [loadBooks]);
 
   const book = books.find((elem) => elem.id === id);
 
+  // Filtre par nom de livre
+  const { useListUsers } = useUsersProviders();
+  const { users, load } = useListUsers();
+  useEffect(() => load, [load]);
+
+  const usersWithBook = users.filter((user) => {
+    if (!book?.name) return true;
+    return user.books.some((titre) => titre === book.name);
+  });
+
   return (
     <main>
-      <h1>Auteur</h1>
-      <p>{`${book?.author.firstName} ${book?.author.lastName}`}</p>
-      <h2>Livre</h2>
+      <h1>Livre</h1>
       <p>
         <strong>Titre:</strong>
         {` ${book?.name} `}
@@ -32,6 +39,12 @@ const BooksDetailsPage: FC = () => {
         <strong>Date d&apos;écriture:</strong>
         {` ${book?.writtenOn}`}
       </p>
+      {usersWithBook.map((user) => (
+        <div key={user.id}>
+          <p>{`${user.firstname} ${user.lastname}`}</p>
+          <a href={`http://localhost:3000/users/${user.id}`}>Details</a>
+        </div>
+      ))}
     </main>
   );
 };
