733731f9f93ee5191c0145532b18566b204e2071
pierre-antoine.gilbert@student.junia.com <pierre-antoine.gilbert@student.junia.com>
2023-11-10 18:00:59 +0100

    Page de détails des auteurs

--- library-site/src/app/authors/[id]/page.tsx
+++ library-site/src/app/authors/[id]/page.tsx
@@ -1,17 +1,31 @@
 'use client';
 
 import { useParams } from 'next/navigation';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useAuthorsProviders } from '@/hooks';
 
 const AuthorDetailsPage: FC = () => {
   const { id } = useParams();
 
+  const { useListAuthors } = useAuthorsProviders();
+  const { authors, load } = useListAuthors();
+  useEffect(() => load, [load]);
+
+  const author = authors.find((elem) => elem.id === id);
+
   return (
-    <>
-      Author details &apos;
-      {id}
-      &apos; not implemented
-    </>
+    <main>
+      <p className="text-lg font-semibold">
+        {`${author?.firstName} ${author?.lastName}`}
+      </p>
+
+      {author?.books.map((book) => (
+        <div key={book.id}>
+          <p>{book.name}</p>
+          <a href={`http://localhost:3000/books/${book.id}`}>Details</a>
+        </div>
+      ))}
+    </main>
   );
 };
 
